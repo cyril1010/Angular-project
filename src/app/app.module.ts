3473fd49ac90064411d905fafb45788daeb9c4bd
Cyril <letter2cyril@gmail.com>
2023-03-02 15:46:24 +0530

    product

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -16,6 +16,8 @@ import { AddproductComponent } from './addproduct/addproduct.component';
 import {MatCardModule} from '@angular/material/card';
 
 import {MatGridListModule} from '@angular/material/grid-list';
+import {MatFormFieldModule} from '@angular/material/form-field';
+
 
 
 
@@ -37,6 +39,7 @@ import {MatGridListModule} from '@angular/material/grid-list';
     BrowserAnimationsModule,
     MatToolbarModule,
     MatCardModule,
+    MatFormFieldModule,
     MatIconModule,
     MatDividerModule,
     MatGridListModule,
