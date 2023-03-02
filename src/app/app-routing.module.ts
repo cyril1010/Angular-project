35bea6e70b2287a1e4f2726291c7f25a4ca5dc83
Cyril <letter2cyril@gmail.com>
2023-03-02 16:15:51 +0530

    'blahh'

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,6 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { NavbarComponent } from './navbar/navbar.component';
 import { AddproductComponent } from './addproduct/addproduct.component';
-import { LoginComponent } from './login/login.component';
 import { HomeComponent } from './home/home.component';
 
 
@@ -10,8 +9,7 @@ import { HomeComponent } from './home/home.component';
 
 const routes: Routes = [
         {path:'',component:HomeComponent},
-        {path:'addproduct',component: AddproductComponent},
-        {path:'login',component: LoginComponent}
+        {path:'addproduct',component: AddproductComponent}
       ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,13 +14,8 @@ import {MatDividerModule} from '@angular/material/divider';
 import { HomeComponent } from './home/home.component';
 import { AddproductComponent } from './addproduct/addproduct.component';
 import {MatCardModule} from '@angular/material/card';
-
 import {MatGridListModule} from '@angular/material/grid-list';
 import {MatFormFieldModule} from '@angular/material/form-field';
-
-
-
-
 
 
 
