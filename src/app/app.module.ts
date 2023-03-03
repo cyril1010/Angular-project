2a09686fe926ac2a8cd739988abe125a994df28c
Cyril <letter2cyril@gmail.com>
2023-03-03 10:00:35 +0530

    'asher'

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -16,6 +16,8 @@ import { AddproductComponent } from './addproduct/addproduct.component';
 import {MatCardModule} from '@angular/material/card';
 import {MatGridListModule} from '@angular/material/grid-list';
 import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatInputModule} from '@angular/material/input';
+
 
 
 
@@ -31,6 +33,7 @@ import {MatFormFieldModule} from '@angular/material/form-field';
     BrowserModule,
     AppRoutingModule,
     MatSlideToggleModule,
+    MatInputModule,
     BrowserAnimationsModule,
     MatToolbarModule,
     MatCardModule,
